7d0128fdc331b24cb2e54b084693bef7fe5f5940
lewis-otwori <lewisotwori@gmail.com>
2023-03-25 13:09:15 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", () => {})
+  const typeForm = document.querySelector('#new-task-description')
+  const form = document.getElementById('create-task-form')
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    alert('Thank you for the feedback!');
+  const element = document.createElement('li')
+  element.textContent = `${typeForm.value}`
+  const div = document.getElementById('list')
+  div.appendChild(element)
+  const btn = document.createElement('button')
+  btn.textContent = `X`;
+  btn.addEventListener('click',function() {
+    element.parentElement.removeChild(element)
+  })
+  element.appendChild(btn);
+  });
